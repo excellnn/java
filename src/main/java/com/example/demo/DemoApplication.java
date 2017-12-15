8611e33b63d95eac548b0a696d9d674d87f58ee7
lnn <liu.cengjingcanghai@gmail.com>
2017-12-15 16:51:03 +0800

    传递模型数据到视图

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,10 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class DemoApplication {
-
 	public static void main(String[] args) {
 		System.setProperty("spring.devtools.restart.enabled", "true");
 		SpringApplication.run(DemoApplication.class, args);
--- src/main/java/com/example/demo/spittr/data/SpittleRepository.java
+++ src/main/java/com/example/demo/spittr/data/SpittleRepository.java
@@ -1,7 +1,26 @@
 package com.example.demo.spittr.data;
 
+import com.example.demo.spittr.Spittle;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
-public interface SpittleRepository {
-    List<Spittle> findSpittles(long max, int count);
+@Component
+public class SpittleRepository {
+    public List<Spittle> findSpittles(long max, int count){
+        ArrayList list = new ArrayList<Spittle>();
+        Calendar calendar = Calendar.getInstance();
+
+        calendar.set(2009, 6 - 1, 12);
+
+        System.out.println(calendar.get(Calendar.YEAR));
+
+        list.add(new Spittle(1L,"first",new Date()));
+        list.add(new Spittle(2L,"second",new Date()));
+        list.add(new Spittle(3L,"third",new Date()));
+        return list;
+    }
 }
